5eb02c2bc5fcbdea62e40215412c4aebaa42ec26
Aidin Omurkulov <omurkulovaidin94@gmail.com>
2023-02-23 13:15:32 +0600

    deleted unnecessary folder

--- src/MainRoutes.js
+++ src/MainRoutes.js
@@ -4,15 +4,12 @@ import { Route, Routes } from "react-router-dom";
 //Custom
 import TagFile from "./Components/TagFile";
 import Diagnostic from "./Components/Diagnostic";
-import TestCalculateMM from "./Components/TestCalculateMM";
-import MouseCoordinates from "./Components/TestCalculateMM";
 
 const MainRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<TagFile />} />
       <Route path="/trg-diagnostic" element={<Diagnostic />} />
-      <Route path="testMM" element={<MouseCoordinates />} />
       <Route />
     </Routes>
   );
