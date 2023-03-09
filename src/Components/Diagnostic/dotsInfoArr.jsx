06ae832fa6383ee010175e550354c828aa255f76
Aidin Omurkulov <omurkulovaidin94@gmail.com>
2023-03-09 18:58:22 +0600

    Поставил картинки, только не у всех точек

--- src/Components/Diagnostic/dotsInfoArr.jsx
+++ src/Components/Diagnostic/dotsInfoArr.jsx
@@ -1,102 +1,136 @@
-const img = "https://api.time.com/wp-content/uploads/2019/08/better-smartphone-photos.jpg?quality=85&w=1024&h=628&crop=1"
+const img =
+  "https://api.time.com/wp-content/uploads/2019/08/better-smartphone-photos.jpg?quality=85&w=1024&h=628&crop=1";
 export const DOTS_INFO = [
-    {
-        name: "firstDot", img,
-    },
-    {
-        name: "secondDot", img,
-    },
-    {
-        name: "S", img,
-    },
-    {
-        name: "Se", img,
-    },
-    {
-        name: "N", img,
-    },
-    {
-        name: "A", img,
-    },
-    {
-        name: "B", img,
-    },
-    {
-        name: "is", img,
-    },
-    {
-        name: "ias", img,
-    },
-    {
-        name: "ii", img,
-    },
-    {
-        name: "iai", img,
-    },
-    {
-        name: "U6", img,
-    },
-    {
-        name: "L6", img,
-    },
-    {
-        name: "Pt", img,
-    },
-    {
-        name: "Po (Pr)", img,
-    },
-    {
-        name: "Or", img,
-    },
-    {
-        name: "Sna", img,
-    },
-    {
-        name: "Snp", img,
-    },
-    {
-        name: "Go", img,
-    },
-    {
-        name: "Gn", img,
-    },
-    {
-        name: "Me", img,
-    },
-    {
-        name: "Ba", img,
-    },
-    {
-        name: "Ar", img,
-    },
-    {
-        name: "Pg", img,
-    },
-    {
-        name: "Co", img,
-    },
-    {
-        name: "gl", img,
-    },
-    {
-        name: "sn", img,
-    },
-    {
-        name: "pg=pog", img,
-    },
-    {
-        name: "st", img,
-    },
-    {
-        name: "me", img,
-    },
-    {
-        name: "col=pn", img,
-    },
-    {
-        name: "UL", img,
-    },
-    {
-        name: "LL", img,
-    },
+  {
+    name: "firstDot",
+    img: "",
+  },
+  {
+    name: "secondDot",
+    img,
+  },
+  {
+    name: "S",
+    img: "https://diagnortho.ru/assets/template/img/S.jpg",
+  },
+  {
+    name: "Se",
+    img: "https://diagnortho.ru/assets/template/img/Se.jpg",
+  },
+  {
+    name: "N",
+    img: "https://diagnortho.ru/assets/template/img/N.jpg",
+  },
+  {
+    name: "A",
+    img: "https://diagnortho.ru/assets/template/img/A.jpg",
+  },
+  {
+    name: "B",
+    img: "https://diagnortho.ru/assets/template/img/B.jpg",
+  },
+  {
+    name: "is",
+    img: "",
+  },
+  {
+    name: "ias",
+    img,
+  },
+  {
+    name: "ii",
+    img: "https://diagnortho.ru/assets/template/img/ii.jpg",
+  },
+  {
+    name: "iai",
+    img,
+  },
+  {
+    name: "U6",
+    img,
+  },
+  {
+    name: "L6",
+    img,
+  },
+  {
+    name: "Pt",
+    img,
+  },
+  {
+    name: "Po (Pr)",
+    img,
+  },
+  {
+    name: "Or",
+    img,
+  },
+  {
+    name: "Sn   a",
+    img,
+  },
+  {
+    name: "Snp",
+    img,
+  },
+  {
+    name: "Go",
+    img: "https://diagnortho.ru/assets/template/img/Go.jpg",
+  },
+  {
+    name: "Gn",
+    img: "https://diagnortho.ru/assets/template/img/Gn.jpg",
+  },
+  {
+    name: "Me",
+    img: "https://diagnortho.ru/assets/template/img/Me.jpg",
+  },
+  {
+    name: "Ba",
+    img,
+  },
+  {
+    name: "Ar",
+    img,
+  },
+  {
+    name: "Pg",
+    img,
+  },
+  {
+    name: "Co",
+    img: "https://diagnortho.ru/assets/template/img/Co.jpg",
+  },
+  {
+    name: "gl",
+    img,
+  },
+  {
+    name: "sn",
+    img,
+  },
+  {
+    name: "pg=pog",
+    img,
+  },
+  {
+    name: "st",
+    img,
+  },
+  {
+    name: "me",
+    img,
+  },
+  {
+    name: "col=pn",
+    img,
+  },
+  {
+    name: "UL",
+    img,
+  },
+  {
+    name: "LL",
+    img,
+  },
 ];
